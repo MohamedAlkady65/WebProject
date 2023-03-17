dc541cdf1c9ae4f49ad65c7d075bab2c8482ad5c
MohamedAlkady65 <128157617+MohamedAlkady65@users.noreply.github.com>
2023-03-17 16:48:42 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
-document.querySelector("nav .shop").onclick =()=>{
-    document.querySelector("nav .cat").classList.toggle("active");
-}
-document.querySelector("nav .menu").onclick =()=>{
-    document.querySelector("nav .btns").classList.toggle("active");
-    document.querySelector(".cart").classList.remove("active");
-    document.querySelector(".overlay").classList.remove("active")
-}
+let shopBtn=document.querySelector("nav .shop");
+let catList=document.querySelector("nav .cat");
+let menuBtn=document.querySelector("nav .menu");
+let navList=document.querySelector("nav .list");
+let cart=document.querySelector(".cart");
+let overlay=document.querySelector(".overlay");
+let cartIcon=document.querySelector("nav .cart-icon");
+let closeCart=document.querySelector(".cart .close");
 
+
+
+
+shopBtn.onclick =()=>{
+    catList.classList.toggle("active");
+}
+menuBtn.onclick =()=>{
+    navList.classList.toggle("active");
+    cart.classList.remove("active");
+    overlay.classList.remove("active")
+}
 document.onclick=(e)=>{
     if(e.target!=document.querySelector("nav .shop")&&!e.target.classList.contains("cat-item"))
-    document.querySelector("nav .cat").classList.remove("active");
+    catList.classList.remove("active");
 }
-
-document.querySelector("nav .cart-icon").onclick =()=>{
-    document.querySelector(".cart").classList.toggle("active");
-    document.querySelector(".overlay").classList.toggle("active");
-    document.querySelector("nav .btns").classList.remove("active");
-
+cartIcon.onclick =()=>{
+    cart.classList.toggle("active");
+    overlay.classList.toggle("active");
+    navList.classList.remove("active");
 }
-
-document.querySelector(".cart .close").onclick =()=>{
-    document.querySelector(".cart").classList.remove("active");
-    document.querySelector(".overlay").classList.remove("active")
+closeCart.onclick =()=>{
+    cart.classList.remove("active");
+    overlay.classList.remove("active")
 }
 
