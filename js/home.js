8751c0ad96e710af9b666ff2ca555ff7ba70c084
MohamedAlkady65 <128157617+MohamedAlkady65@users.noreply.github.com>
2023-04-25 20:56:12 +0200

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -5,8 +5,6 @@ let menuList=document.querySelector(".header .down");
 
 
 
-
-
 menuBtn.onclick =()=>{
     menuList.classList.toggle("active");
 }
@@ -20,4 +18,44 @@ document.onclick=(e)=>{
         catList.classList.remove("active");
     }
 }
+
+let oneProCat = document.querySelector(".categories .container .one-pro");
+let nextCat = document.querySelectorAll(".next-cat");
+let preCat = document.querySelectorAll(".pre-cat");
+
+nextCat.forEach((ele)=>{
+    ele.onclick = ()=>{
+
+
+
+        let par =ele.parentElement.getBoundingClientRect().right;
+        let last = ele.previousElementSibling.firstElementChild.lastElementChild.getBoundingClientRect().right;
+
+        
+
+        if(last>par)
+        {
+            let curTra = parseFloat(ele.previousElementSibling.firstElementChild.style.translate || 0) ;
+        curTra-=oneProCat.offsetWidth+12;
+        ele.previousElementSibling.firstElementChild.style.translate = curTra+"px";
+        }
+        
+    }
+})
+
+
+preCat.forEach((ele)=>{
+    ele.onclick = ()=>{
+
+      let curTra = parseFloat(ele.nextElementSibling.firstElementChild.style.translate || 0) ;
+        curTra+=oneProCat.offsetWidth+12;
+        if(curTra <= 0)
+        ele.nextElementSibling.firstElementChild.style.translate = curTra+"px";
+
+
+    }
+})
+
+
+
 
---
+++ js/sign.js
@@ -0,0 +1,16 @@
+const forms=document.querySelector('.forms'),
+    links=document.querySelectorAll('.link');
+
+
+
+links.forEach(link => {
+link.addEventListener("click", e=>{
+e.preventDefault();
+forms.classList.toggle("show-signup");
+
+
+
+})
+
+
+});    
